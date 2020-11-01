2dca2894a12cc49eb5e30f4a9c5c2494bd96b565
Dhananjayan <dhananjayan322@gmail.com>
2020-11-01 11:13:03 +0530

    Travis CI file updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Black box!!!</p>
+          <p>
+          Hello Black box!!!
+        </p>
       </header>
     </div>
   );
